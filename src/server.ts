0dff99ff7edcd9cf3e8574d72dcad4f343ff67e6
Ekhlasur Rahman <ekhlasur.ru@outlook.com>
2025-12-01 07:15:18 +0600

    Add CRUD for users data

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { Pool } from "pg";
+import { Pool, Result } from "pg";
 import dotenv from "dotenv";
 import path from "path";
 
@@ -55,6 +55,110 @@ app.post("/", (req: Request, res: Response) => {
     success: true,
     message: "API is ready",
   });
+});
+
+//Users create
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name,email) VALUES($1,$2) RETURNING *`,
+      [name, email]
+    );
+    res.status(200).json({
+      success: true,
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(400).send("Invalid input");
+  }
+});
+//Users GET
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(400).send("Invalid input GET");
+  }
+});
+//Users GET by ID
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    // console.log(req.params.id);
+    const result = await pool.query(`SELECT * FROM users WHERE id=$1`, [
+      req.params.id,
+    ]);
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not Found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User Fetch Successfully",
+        data: result.rows[0],
+      });
+    }
+    // console.log(result.rows);
+  } catch (error: any) {
+    res.status(400).send("Invalid input GET by id");
+  }
+});
+//update user
+app.put("/users/:id", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      `UPDATE users SET name=$1, email=$2 WHERE  id=$3 RETURNING *`,
+      [name, email, req.params.id]
+    );
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not Found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User Updated Successfully",
+        data: result.rows[0],
+      });
+    }
+  } catch (error: any) {
+    res.status(400).send("Invalid input Update by id");
+  }
+});
+//Delete user
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+        data: result.rows,
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 app.listen(port, () => {
